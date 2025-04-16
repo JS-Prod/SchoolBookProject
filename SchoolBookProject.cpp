44054890e8920a5af5000162d8ef770f98435f82
Joshua Simpson <joshuasimpsonproductions@gmail.com>
2025-04-16 04:06:58 -0700

    create array of 5 books

--- SchoolBookProject.cpp
+++ SchoolBookProject.cpp
@@ -3,11 +3,12 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
-
-    Book book1("To Kill a Mockingbird", "Harper Lee", "1kd130vc");
-    Book book2("The Great Gatsby", "F. Scott Fitzgerald", "8dsb2bcv7");
-    Book book3("Pride and Prejudice", "Jane Austin", "4a7a37d3g");
-        
+    Book books[5] = {
+        Book("To Kill a Mockingbird", "Harper Lee", "0"),
+        Book("The Great Gatsby", "F. Scott Fitzgerald", "1"),
+        Book("Pride and Prejudice", "Jane Austin", "2"),
+        Book("1984", "George Orwell", "3"),
+        Book("Moby Dick", "Herman Melville", "4")
+    };    
 }
 
